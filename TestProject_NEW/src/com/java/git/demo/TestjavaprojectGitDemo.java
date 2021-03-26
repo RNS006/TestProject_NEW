0abde8ab5fd004ba74ae546b781aca1ae01766af
rudra006-admin <rudrag.scm@gmail.com>
2021-03-26 17:26:32 +0530

    commit the changes

---
+++ TestProject_NEW/src/com/java/git/demo/TestjavaprojectGitDemo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.git.demo;
+
+/**
+ * @author Rudra
+ *
+ */
+public class TestjavaprojectGitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hi..Welcome To Git Sample Java Program...");
+
+	}
+
+}
